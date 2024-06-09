3dd9ff1689e2dec7ee7b6e224c2b3d1dfb093d92
wanghonghui <wanghonghui1998@gmail.com>
2024-06-09 12:41:29 +0000

    feat: grid channel

--- bash_dynamics/navier-stokes/inr_wf_r3_S256_T64_l40_grid_bs128_woscale_128sub0_2_channel.sh
+++ bash_dynamics/navier-stokes/inr_wf_r3_S256_T64_l40_grid_bs128_woscale_128sub0_2_channel.sh
@@ -15,7 +15,7 @@ set -x
 # conda activate coral
 dir='/cluster/nvme4a/whh/dataset/ns_nms/u_Re800_T60_transient_tr256.npy'
 dataset_name='navier-stokes-nms-40-64-wonorm'
-model_type='siren_grid'
+model_type='siren_grid_chan'
 same_grid=True
 sub_from=2
 sub_tr=0.2
@@ -33,6 +33,6 @@ grid_size=$2
 modulate_scale=False
 
 saved_checkpoint=False
-name='grid'$grid_size'_woscale_bs128_wf_r3_last40_T64_128sub0_2-wonorm'
-id='grid'$grid_size'_woscale_bs128_wf_r3_S256_last40_T64_128sub0_2-wonorm'
+name='gridchan'$grid_size'_woscale_bs64_wf_r3_last40_T64_128sub0_2-wonorm'
+id='gridchan'$grid_size'_woscale_bs64_wf_r3_S256_last40_T64_128sub0_2-wonorm'
 python3 inr/inr.py "inr.use_norm=$use_norm" "inr.grid_size=$grid_size" "inr.modulate_scale=$modulate_scale" "data.dir=$dir" "data.sub_from=$sub_from" "data.same_grid=$same_grid" "data.dataset_name=$dataset_name" "inr.model_type=$model_type" "data.seq_inter_len=$seq_inter_len" "data.seq_extra_len=$seq_extra_len" "data.sub_tr=$sub_tr" "data.sub_te=$sub_te" "optim.lr_inr=$lr_inr" "inr.depth=$depth" "inr.latent_dim=$latent_dim" "inr.hidden_dim=$hidden_dim" "optim.batch_size=$batch_size" "optim.epochs=$epochs" "wandb.saved_checkpoint=$saved_checkpoint" "wandb.name=$name" "wandb.id=$id" #"wandb.dir=$dir" "wandb.checkpoint_path=$checkpoint_path"
--- bash_dynamics/navier-stokes/inr_wf_r3_S256_T64_l40_grid_bs128_woscale_128sub0_2_code2fourierc_debug.sh
+++ bash_dynamics/navier-stokes/inr_wf_r3_S256_T64_l40_grid_bs128_woscale_128sub0_2_code2fourierc_debug.sh
@@ -22,7 +22,7 @@ sub_tr=0.2
 sub_te=0.2
 seq_inter_len=20
 seq_extra_len=20
-batch_size=128
+batch_size=64
 lr_inr=0.000005
 epochs=10000
 latent_dim=$1
@@ -31,9 +31,9 @@ hidden_dim=128
 use_norm=False
 grid_size=$2
 modulate_scale=False
-lr_code=1e-2 
+lr_code=1e-2
 
 saved_checkpoint=False
-name='debug_code2cossum'$grid_size'_woscale_bs128_wf_r3_last40_T64_128sub0_2-wonorm_1e-2'
-id='debug_code2fcossum'$grid_size'_woscale_bs128_wf_r3_S256_last40_T64_128sub0_2-wonorm_1e-2'
+name='debug_code2cosmean'$grid_size'_woscale_bs64_wf_r3_last40_T64_128sub0_2-wonorm_1e-2'
+id='debug_code2cosmean'$grid_size'_woscale_bs64_wf_r3_S256_last40_T64_128sub0_2-wonorm_1e-2'
 python3 inr/inr.py "optim.lr_code=$lr_code" "inr.use_norm=$use_norm" "inr.grid_size=$grid_size" "inr.modulate_scale=$modulate_scale" "data.dir=$dir" "data.sub_from=$sub_from" "data.same_grid=$same_grid" "data.dataset_name=$dataset_name" "inr.model_type=$model_type" "data.seq_inter_len=$seq_inter_len" "data.seq_extra_len=$seq_extra_len" "data.sub_tr=$sub_tr" "data.sub_te=$sub_te" "optim.lr_inr=$lr_inr" "inr.depth=$depth" "inr.latent_dim=$latent_dim" "inr.hidden_dim=$hidden_dim" "optim.batch_size=$batch_size" "optim.epochs=$epochs" "wandb.saved_checkpoint=$saved_checkpoint" "wandb.name=$name" "wandb.id=$id" #"wandb.dir=$dir" "wandb.checkpoint_path=$checkpoint_path"
